4a38080b68ff2833a83d549766c6db68ed2f435c
leandrofernandes5 <fernandesleandro1996@gmail.com>
2025-04-16 09:12:25 -0500

    remove response handler, not to be implemented atm

--- src/response_handler.py
+++
@@ -1,7 +0,0 @@
-def format_response(raw_text: str) -> str:
-    """
-    Example function for post-processing the AI model's output.
-    Could include content filtering, removing sensitive info, etc.
-    """
-    # For now, we just return the raw text.
-    return raw_text
